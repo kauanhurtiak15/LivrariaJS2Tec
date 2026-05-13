88ceb2258597c60f5ed7f7437d0b4adb8ed1610b
kauanhurtiak15 <kauan.hurtiak.santos@escola.pr.gov.br>
2026-05-13 12:39:57 +0000

    OK

---
+++ listaLivros.js
@@ -0,0 +1,29 @@
+let precoLivros = [
+
+{
+    titulo: "Harry Potter",
+    preco: 50
+
+    },
+    {
+        titulo: "A Coisa",
+        preco: 80
+    },
+
+    {
+        titulo: "Olhos Prateados",
+        preco: 43
+    },
+
+    {
+        titulo: "Mergulho na Escuridão",
+        preco: 27
+    },
+
+    {
+        titulo: "Os Distorcidos",
+        preco: 50
+    }
+]
+
+module.exports = precoLivros;
